46848513d352c4822404091e862c08e48d2e72e8
Gimna Katugampala <autoservicelk@gmail.com>
2021-07-28 00:52:49 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router,Route,Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
     <div className="App">
        <Navbar/>
+       <Footer/>
     </div>
     </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h3>Copyrights &copy; 2021 - React Blogger</h3>
+            <p>Gimna Katugampala Production</p>
+        </div>
+    )
+}
+
+export default Footer
